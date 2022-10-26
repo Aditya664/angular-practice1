8bc65f7efdf294a565cbdfb3f735893a6e196e17
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2022-10-26 16:16:54 +0530

    Create constvsletvsvar.ts

---
+++ constvsletvsvar.ts
@@ -0,0 +1,22 @@
+
+//both type and initial value
+ 
+var width:number = 100;
+let height:number = 200;
+const key:string = 'abc123';
+ 
+//without type, but with an initial value
+var width = 100;
+let height = 200;
+const key = 'abc123';
+ 
+//only the type
+var width:number;
+let height:number;
+//const   does not support without initial value
+ 
+//without Type/Initial Value
+var width;
+let height;
+//const   does not support without initial value
+ 
